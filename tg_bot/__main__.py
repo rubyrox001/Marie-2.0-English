1f8991f40784a604b859ea029132ebf53377cf58
rubyrox001 <59814978+rubyrox001@users.noreply.github.com>
2020-01-13 10:18:53 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -20,12 +20,7 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 PM_START_TEXT = """
 hoi {}, my name is {}! if you have any questions about how to use me please give me /help... 
 
-im a group manager bot maintained by  [this person](tg://user?id={}).
-
-
-
-
-
+I'm a group manager bot maintained by  [this person](tg://user?id={}).
 
 For more commands click /help...
 
