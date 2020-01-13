a481a6e9b122edc2c51cba7b9f2d9561cc7a80ed
rubyrox001 <59814978+rubyrox001@users.noreply.github.com>
2020-01-13 19:24:05 +0530

    Update translator.py

--- tg_bot/modules/translator.py
+++ tg_bot/modules/translator.py
@@ -1,33 +1,25 @@
-from typing import Optional, List
 
 from telegram import Message, Update, Bot, User
-from telegram import MessageEntity
 from telegram.ext import Filters, MessageHandler, run_async
 
-from tg_bot import dispatcher, LOGGER
+from requests import get
+
 from tg_bot.modules.disable import DisableAbleCommandHandler
+from tg_bot import dispatcher
 
-from googletrans import Translator
-
+base_url = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
+api_key = 'trnsl.1.1.20180603T023816Z.763b39e3388b46d6.aa9abf45baceb438c96bb1593ce58199cc66c4f1'
 
 @run_async
-def do_translate(bot: Bot, update: Update, args: List[str]):
-    short_name = "Created By @MidukkiBot 😬"
-    msg = update.effective_message # type: Optional[Message]
-    lan = " ".join(args)
-    to_translate_text = msg.reply_to_message.text
-    translator = Translator()
-    try:
-        translated = translator.translate(to_translate_text, dest=lan)
-        src_lang = translated.src
-        translated_text = translated.text
-        msg.reply_text("Translated from {} to {}.\n {}".format(src_lang, lan, translated_text))
-    except exc:
-        msg.reply_text(str(exc))
+def translate(bot: Bot, update: Update):
+  message = update.effective_message
+  text = message.reply_to_message.text
+  translation = get(f'{base_url}?key={api_key}&text={text}&lang=en').json()
+  
+  reply_text = f"Language: {translation['lang']}\nText: {translation['text'][0]}"
+  
+  message.reply_to_message.reply_text(reply_text)
 
+translate_handler = DisableAbleCommandHandler("tl", translate)
 
-__help__ = """- /tr (language code) as reply to a long message.
-"""
-__mod_name__ = "Google Translate"
-
-dispatcher.add_handler(DisableAbleCommandHandler("tr", do_translate, pass_args=True))
+dispatcher.add_handler(translate_handler)
