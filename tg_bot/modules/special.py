ec8ad19404af818cd85d8831e4cfa8a1b6b4093d
rubyrox001 <59814978+rubyrox001@users.noreply.github.com>
2020-01-13 18:36:50 +0530

    Update special.py

--- tg_bot/modules/special.py
+++ tg_bot/modules/special.py
@@ -102,31 +102,35 @@ def leavechat(bot: Bot, update: Update, args: List[int]):
         update.effective_message.reply_text("You don't seem to be referring to a chat")
 
 __help__ = """
-**Owner only:**
-- /getlink **chatid**: Get the invite link for a specific chat.
+*Owner only:*
+- /getlink *chatid*: Get the invite link for a specific chat.
 - /banall: Ban all members from a chat
-- /leavechat **chatid** : leave a chat
-**Sudo/owner only:**
-- /quickscope **userid** **chatid**: Ban user from chat.
-- /quickunban **userid** **chatid**: Unban user from chat.
-- /snipe **chatid** **string**: Make me send a message to a specific chat.
-- /rban **userid** **chatid** remotely ban a user from a chat
-- /runban **userid** **chatid** remotely unban a user from a chat
+- /leavechat *chatid* : leave a chat
+
+*Sudo/owner only:*
+- /quickscope *userid* *chatid*: Ban user from chat.
+- /quickunban *userid* *chatid*: Unban user from chat.
+- /snipe *chatid* *string*: Make me send a message to a specific chat.
+- /rban *userid* *chatid* remotely ban a user from a chat
+- /runban *userid* *chatid* remotely unban a user from a chat
 - /Stats: check bot's stats
 - /chatlist: get chatlist
 - /gbanlist: get gbanned users list
 - /gmutelist: get gmuted users list
 - Chat bans via /restrict chat_id and /unrestrict chat_id commands
-**Support user:**
+
+*Support user:*
 - /Gban : Global ban a user
 - /Ungban : Ungban a user
 - /Gmute : Gmute a user
 - /Ungmute : Ungmute a user
 Sudo/owner can use these commands too.
-**Owner only:**
+
+*Owner only:*
 - /banall: Ban all members from a chat
-**SUDO only:**
-- /snipe chatid string: Make me send a message to a specific chat.
+
+*Sudo only:*
+- /snipe *chatid* *string*: Make me send a message to a specific chat.
 """
 __mod_name__ = "Special"
 
