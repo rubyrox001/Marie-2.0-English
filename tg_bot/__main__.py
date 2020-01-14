190f5f44bcfc4d74568a0c47f5785960f879e845
rubyrox001 <59814978+rubyrox001@users.noreply.github.com>
2020-01-14 18:14:10 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -27,7 +27,6 @@ I'm Genius, Billionaire, Playboy, Philanthropist!
 You can find the list of available commands with /help.
 
 Want to add me in your group? [Click here!](https://telegram.me/Rubytony_bot?startgroup=add)
-
 """
 
 HELP_STRINGS = """
